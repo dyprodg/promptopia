47c179349f5173c1a79ffdd1f424a40e1228e2c0
dyprodg <dennisdiepolder@outlook.com>
2023-09-08 18:45:00 +0200

    Bugfix DB connection

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const UserSchema = new Schema({
     username: {
         type: String,
         required: [true, 'Username is required!'],
-        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+        match: [/^(?=.{8,30}$)(?![.])(?!.*[.]{2})[a-zA-Z0-9]+(?:[._][a-zA-Z0-9]+){0,2}$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
       },
     image: {
         type: String,
@@ -21,3 +21,4 @@ const UserSchema = new Schema({
 const User = models.User || model("User", UserSchema);
 
 export default User;
+
