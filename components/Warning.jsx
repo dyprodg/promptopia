2c594d41fa21cf2b2bec2c6814c582dcbc5edcb8
dyprodg <dennisdiepolder@outlook.com>
2023-09-08 19:39:30 +0200

    warning

---
+++ components/Warning.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Warning = () => {
+  return (
+    <div className=''>
+        <h1>**Website Under Development - Use with Caution** <br></br>
+
+Welcome! This website is a showcase of our coding skills and is not a fully operational product!!!</h1>
+    </div>
+  )
+}
+
+export default Warning
