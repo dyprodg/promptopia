2b1fdbfbd92098fba863e563f707addd91acc1b1
dyprodg <dennisdiepolder@outlook.com>
2023-09-08 14:22:39 +0200

    NavBase

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,7 @@ const Nav = () => {
   const isUserLoggedIn = true;
 
   const [providers, setProviders ] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
     const setProviders = async () => {
@@ -80,8 +81,37 @@ const Nav = () => {
                 height={37}
                 className='rounded-full'
                 alt='profile'
-                onClick={() => {}}
-              />
+                onClick={() => setToggleDropdown((prev) => !prev)}
+            />
+                {toggleDropdown && (
+                  <div className='dropdown'>
+                    <Link
+                      href="/profile"
+                      className='dropdown_link'
+                      onClick={() => setToggleDropdown(false)}
+                    >
+                      My Profile
+                    </Link>
+                    <Link
+                      href="/create-prompt"
+                      className='dropdown_link'
+                      onClick={() => setToggleDropdown(false)}
+                    >
+                      Create Prompt
+                    </Link>
+                    <botton
+                      type='button'
+                      onClick={() => {
+                        setToggleDropdown(false);
+                        signOut();
+                      }}
+                      className="mt-5 w-full black_btn"
+                    >
+                      Sign Out
+                    </botton>
+                  </div>
+                )}
+              
           </div>
         ): (
           <>
