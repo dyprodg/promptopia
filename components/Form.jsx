9c05ce7e046b927c17b78fc15496ae21f4ab3855
dyprodg <dennisdiepolder@outlook.com>
2023-09-09 11:26:41 +0200

    formcomponent

--- components/Form.jsx
+++ components/Form.jsx
@@ -13,10 +13,11 @@ const Form = ({ type ,post ,setPost ,submitting ,handleSubmit }) => {
          onSubmit={handleSubmit}
          className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
         >
+          <label>
           <span className='font-satoshi font-semibold text-base text-gray-700'>
             Your AI Prompt
           </span>
-          <textare 
+          <textarea 
           value={post.prompt}
           onChange={(e) => setPost ({...post,
           prompt: e.target.value })}
@@ -24,8 +25,33 @@ const Form = ({ type ,post ,setPost ,submitting ,handleSubmit }) => {
           required
           className="form_textarea"
           />
+          </label>
+          <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Tag
+            <span className='font-normal'>(#product, #webdevelopment, #idea)</span>
+          </span>
+          <input
+          value={post.prompt}
+          onChange={(e) => setPost ({ ...post, tag: e.target.value })}
+          placeholder="#tag"
+          required
+          className="form_input"
+          />
+          </label>
+          <div className='flex-end mx-3 mb-5 gap-4'>
+            <Link href="/" className='text-gray-500 text-sm'>
+              Cancel
+            </Link>
 
-      
+            <button
+             type='submit'
+             disabled={submitting}
+             className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'
+            >
+              {submitting ? `${tyep}...` : type}
+            </button>
+          </div>
         </form>
     </section>
   )
